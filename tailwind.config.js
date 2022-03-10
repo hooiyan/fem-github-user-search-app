bb03ff35d63f057b16f8f5994b23cada4c0db261
hooiyan <hoecoding@gmail.com>
2022-03-10 16:10:58 +0800

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
