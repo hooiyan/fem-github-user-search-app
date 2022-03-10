dbc3aed06457de4c8d07c0fa3770223d8188674c
hooiyan <hoecoding@gmail.com>
2022-03-10 19:31:51 +0800

    Extend colors to tailwind default

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      "sans": ["Space Mono", "sans-serif"],
+    },
+    extend: {
+      color: {
+        lightModePrimaryBlue: "var(--light-mode-primary-blue)",
+        lightModeGray: "var(--light-mode-gray)",
+        lightModeSecondaryBlue: "var(--light-mode-secondary-blue)",
+        lightModeBlack: "var(--light-mode-black)",
+        lightModeWhite: "var(--light-mode-white)",
+        lightModeWhitest: "var(--light-mode-whitest)",
+        darkModePrimaryBlue: "var(--dark-mode-primary-blue)",
+        darkModeWhite: "var(--dark-mode-white)",
+        darkModeBlack: "var(--dark-mode-black)",
+        darkModeDarkBlue: "var(--dark-mode-dark-blue)",
+        myRed: "var(--red)",
+        myPrimaryBlueLighten: "var(--primary-blue-lighten)",
+      }
+    },
   },
   plugins: [],
 }
