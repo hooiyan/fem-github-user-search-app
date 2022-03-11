b71bdf591907c8db65d544304cb1c175332e8eb0
hooiyan <hoecoding@gmail.com>
2022-03-11 09:54:50 +0800

    Update TopHeader

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TopHeader from "./components/TopHeader.vue";
+</script>
+
+<template>
+  <div class="flex flex-col justify-center mt-36">
+    <TopHeader />
+  </div>
+</template>
---
+++ src/components/TopHeader.vue
@@ -0,0 +1,17 @@
+<script setup>
+import IconMoon from "../assets/IconMoon.vue";
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <p class="font-bold text-[2.6rem]">devfinder</p>
+    <aside class="flex items-center cursor-pointer group">
+      <p
+        class="uppercase font-bold text-[13px] tracking-widest text-lightModeLightGray mr-4 group-hover:text-lightModeBlack"
+      >
+        Dark
+      </p>
+      <IconMoon />
+    </aside>
+  </header>
+</template>
