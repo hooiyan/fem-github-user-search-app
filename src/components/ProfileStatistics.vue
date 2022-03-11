4495877b6c4d51045611085bac4d2c9d3b697204
hooiyan <hoecoding@gmail.com>
2022-03-11 13:26:11 +0800

    Style desktop version ProfileStatistics

---
+++ src/components/ProfileStatistics.vue
@@ -0,0 +1,20 @@
+<template>
+  <section
+    class="flex justify-between px-[32px] py-[15px] ml-[154px] bg-lightModeWhite rounded-[10px]"
+  >
+    <div class="flex flex-col">
+      <p class="text-[13px] text-lightModeSecondaryBlue">Repos</p>
+      <p class="font-bold text-[22px]">8</p>
+    </div>
+
+    <div class="flex flex-col">
+      <p class="text-[13px] text-lightModeSecondaryBlue">Followers</p>
+      <p class="font-bold text-[22px]">3938</p>
+    </div>
+
+    <div class="flex flex-col">
+      <p class="text-[13px] text-lightModeSecondaryBlue">Following</p>
+      <p class="font-bold text-[22px]">9</p>
+    </div>
+  </section>
+</template>
