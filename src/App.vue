9931a70ae87b06a876b711efb755362a54a1e7fb
hooiyan <hoecoding@gmail.com>
2022-03-11 10:34:28 +0800

    Style SearchBar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TopHeader from "./components/TopHeader.vue";
+import SearchBar from "./components/SearchBar.vue";
 </script>
 
 <template>
   <div class="flex flex-col justify-center mt-36">
     <TopHeader />
+    <SearchBar />
   </div>
 </template>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import IconSearch from "../assets/IconSearch.vue";
+</script>
+
+<template>
+  <form
+    class="drop-shadow=[hsla(227, 46%, 51%, 0.2)] flex items-center relative mt-[36px] mb-[24px]"
+  >
+    <IconSearch />
+    <input
+      type="text"
+      name="username"
+      id="username"
+      placeholder="Search GitHub username..."
+      class="py-[22px] pl-[80px] w-full rounded-2xl focus:outline-none placeholder:text-lightModeSecondaryBlue text-lightModeBlack caret-lightModePrimaryBlue text-[18px]"
+    />
+    <input
+      type="submit"
+      value="Search"
+      class="text-lightModeWhitest bg-lightModePrimaryBlue font-bold py-[12px] px-[24px] rounded-[10px] absolute right-0 translate-x-[-10px] cursor-pointer hover:bg-myPrimaryBlueLighten"
+    />
+  </form>
+</template>
