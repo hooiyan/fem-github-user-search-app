a5477bc43ef5f6b97bc69f0bb1ed4aa9914c39e3
hooiyan <hoecoding@gmail.com>
2022-03-11 13:12:33 +0800

    Change to max-width breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
     fontFamily: {
       "sans": ["Space Mono", "sans-serif"],
     },
+    screens: {
+      "2xl": {"max": "1535px"},
+      "xl": {"max": "1279px"},
+      "lg": {"max": "1023px"},
+      "md": {"max": "767px"},
+      "sm": {"max": "639px"},
+    },
     extend: {
       colors: {
         lightModePrimaryBlue: "var(--light-mode-primary-blue)",
