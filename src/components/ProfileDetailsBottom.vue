d43a4dcee721ad5271a1ec96d2f5c3840b252a8c
hooiyan <hoecoding@gmail.com>
2022-03-11 16:11:35 +0800

    Fix text overflow issue

--- src/components/ProfileDetailsBottom.vue
+++ src/components/ProfileDetailsBottom.vue
@@ -7,32 +7,34 @@ import IconCompany from "../assets/IconCompany.vue";
 
 <template>
   <section
-    class="grid grid-cols-2 grid-rows-2 gap-y-[15px] gap-x-[50px] ml-[154px]"
+    class="grid grid-cols-2 grid-rows-2 gap-y-[15px] gap-x-[40px] ml-[154px]"
   >
     <div class="flex items-center">
       <IconLocation />
-      <p class="text-lightModeSecondaryBlue text-[1.5rem] pl-[16px]">
+      <p class="text-lightModeSecondaryBlue text-[1.5rem] ml-[16px]">
         San Francisco
       </p>
     </div>
 
     <div class="flex items-center">
       <IconTwitter />
-      <a class="text-lightModeSecondaryBlue text-[1.5rem] pl-[16px]" href="#"
+      <a class="text-lightModeSecondaryBlue text-[1.5rem] ml-[16px]" href="#"
         >Not Available</a
       >
     </div>
 
     <div class="flex items-center">
       <IconWebsite />
-      <a class="text-lightModeSecondaryBlue text-[1.5rem] pl-[16px]" href="#"
-        >https://github.blog</a
+      <a
+        class="text-lightModeSecondaryBlue text-[1.5rem] ml-[16px] text-ellipsis overflow-hidden"
+        href="#"
+        >https://github.blogffffffffffffffff</a
       >
     </div>
 
     <div class="flex items-center">
       <IconCompany />
-      <p class="text-lightModeSecondaryBlue text-[1.5rem] pl-[16px]">@github</p>
+      <p class="text-lightModeSecondaryBlue text-[1.5rem] ml-[16px]">@github</p>
     </div>
   </section>
 </template>
