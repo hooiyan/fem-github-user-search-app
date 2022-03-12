329e489b06e0cc41d59b429e36dfed22586b4186
hooiyan <hoecoding@gmail.com>
2022-03-12 18:51:16 +0800

    Make SearchBar responsive

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -12,12 +12,12 @@ import IconSearch from "../assets/IconSearch.vue";
       name="username"
       id="username"
       placeholder="Search GitHub username..."
-      class="py-[22px] pl-[80px] w-full rounded-2xl focus:outline-none placeholder:text-lightModeSecondaryBlue text-lightModeBlack caret-lightModePrimaryBlue text-[1.8rem] xs:text-[1.3rem]"
+      class="py-[22px] pl-[80px] 2xs:pl-[40px] w-full rounded-2xl focus:outline-none placeholder:text-lightModeSecondaryBlue text-lightModeBlack caret-lightModePrimaryBlue text-[1.8rem] xs:text-[1.3rem] 2xs:text-[1.2rem]"
     />
     <input
       type="submit"
       value="Search"
-      class="text-lightModeWhitest bg-lightModePrimaryBlue font-bold py-[12px] px-[24px] rounded-[10px] absolute right-0 translate-x-[-10px] cursor-pointer hover:bg-myPrimaryBlueLighten xs:text-[1.4rem]"
+      class="text-lightModeWhitest bg-lightModePrimaryBlue font-bold py-[12px] px-[24px] 2xs:px-[14px] rounded-[10px] absolute right-0 translate-x-[-10px] cursor-pointer hover:bg-myPrimaryBlueLighten xs:text-[1.4rem]"
     />
   </form>
 </template>
